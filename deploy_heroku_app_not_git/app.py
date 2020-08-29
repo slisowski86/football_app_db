0145865b1c843810f4cc8e8adf12e24607d5c0e6
Slawomir Lisowski <slisowski@gmail.com>
2020-08-29 01:21:19 +0100

    Nowy layout

--- deploy_heroku_app_not_git/app.py
+++ deploy_heroku_app_not_git/app.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Fri Aug 21 18:10:10 2020
 
@@ -9,11 +10,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table as dt
-from dash.dependencies import Input, Output, State, MATCH, ALL
-import copy
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import psycopg2
 import numpy as np
 import pandas as pd
-import psycopg2
 
 
 
@@ -21,17 +22,22 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app=dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+
+
 server = app.server
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-#psycopg2.connect(host="localhost",database="FootballStats",user="postgres",password="Faraon86@")
-#psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+
 
 
 players=[]
 nationality=[]
 clubs=[]
+score=[]
+stats=[]
 
 try:
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -42,6 +48,12 @@ try:
     nationality=cursor.fetchall()
     cursor.execute("SELECT club_name FROM clubs")
     clubs=cursor.fetchall()
+    cursor.execute("Select * FROM player_score LIMIT 0")
+    
+    score = [desc[0] for desc in cursor.description]
+    cursor.execute("Select * FROM player_stats LIMIT 0")
+    
+    stats = [desc[0] for desc in cursor.description]
     conn.commit()
     
     cursor.close()
@@ -56,33 +68,18 @@ finally:
 club_unique=np.unique(clubs)
 nationality_unique=np.unique(nationality)
 
-sql_dict={}
-sql_temp_dict={}
-result={}
+
+score.remove('player_id')
+stats.remove('player_id')
 
 
 
 
 
-data_to_report=['age', 'appearances', 'mins', 'goals',
-                'assists', 'yel', 'red', 'shots_per_game', 'pass_score',
-                'aerialswon', 'man_of_the_match', 'rating', 'perfdef', 'perfattack', 'perfposs',
-                'total']
 
-
-
-club_nat_dict={'Narodowość': nationality_unique,
-               'Klub': club_unique}
-
-
-sql_schema_dict={'players':['id', 'name','age','nationality_id','club_id'],
-          'nations':['id', 'nationality_name'],
-          'clubs':['id', 'club_name'],
-          'player_score':['player_id','appearances','mins', 'goals',
-                          'assists','yel','red','shots_per_game','pass_score',
-                          'aerialswon','man_of_the_match','rating'],
-          'player_stats':['player_id','perfdef','perfattack','perfposs','total']}
 app.layout = html.Div(children=[
+    
+   
     html.H2(children="Aplikacja Football Stats", style={"background-color":"tomato"}),
     html.H3(children="Wyszukaj statystyki według imienia i nazwiska zawodnika", style={'border-style':'solid'}),
     html.Datalist(
@@ -99,48 +96,111 @@ app.layout = html.Div(children=[
     html.Div(id='my_output', style={'display':'none'}),
     html.Table(id='table'),
     html.H3(children="Wyszukaj zawodników", style={'border-style':'solid'}),
+    html.H4(children="Wiek"),
+    dcc.Dropdown(id='age_value_sign',
+                          options=[
+            {'label': 'more_than', 'value': '>'},
+            {'label': 'equal', 'value': '='},
+            {'label': 'less_than', 'value': '<'}
+        ],
+        placeholder="wybierz filtr",
+        style={'width':'40%'}),
+    dcc.Input(id='age_value',
+                          type='text',
+                          value='',
+                          
+                            placeholder='Wpisz wartosć'),
+    html.Button('Wstaw',id='age_button', n_clicks=0),
     html.H4(children="Narodowość"),
     dcc.Dropdown(id="nations_dd",
                  options=[
                      {'label': i, 'value': i} for i in nationality_unique
                  ],
+                 placeholder="wybierz narodowosć",
+                 style={'width':'40%'}
 
 
                  ),
-   
-    html.Button('Wstaw',id='nations_dd_button', n_clicks=0),
-    html.Div(id='nations_out'),
+    
+    html.Button('Wstaw',id='nations_dd_button',n_clicks=0),
+        
     html.H4(children="Klub"),
     dcc.Dropdown(id="clubs_dd",
                  options=[
                      {'label': i, 'value': i} for i in club_unique
                  ],
+                 placeholder="wybierz klub",
+                 style={'width':'40%'}
 
 
                  ),
     html.Button('Wstaw',id='clubs_dd_button', n_clicks=0),
-    html.Div(id='clubs_out'),
-    html.H4(children="Statystyki"),
-    dcc.Dropdown(id="stats_dd",
+    
+    html.H4(children="Punktacja"),
+    dcc.Dropdown(id="score_dd",
                  options=[
-                     {'label': i, 'value': i} for i in data_to_report
+                     {'label': i, 'value': i} for i in score
                  ],
+                 placeholder="wybierz",
+                 style={'width':'40%'}
 
 
                  ),
-    html.Button('Wstaw',id='stats_dd_button', n_clicks=0),
+    dcc.Dropdown(id='score_value_sign',
+                          options=[
+            {'label': 'more_than', 'value': '>'},
+            {'label': 'equal', 'value': '='},
+            {'label': 'less_than', 'value': '<'}
+        ],
+        placeholder="wybierz filtr",
+        style={'width':'40%'}),
     
-    html.Div(id='stats_container'),
-    html.Button('Pokaż filtry',id='show_filters', n_clicks=0),
-    html.Div(id='filter_table'),
-    html.Div(id='stats_table'),
+    
+    dcc.Input(id='score_value',
+                          type='text',
+                          value='',
+                          
+                            placeholder='Wpisz wartosć'),
+    html.Button('Wstaw',id='score_dd_button', n_clicks=0),
+    html.H4(children="Statystyki"),
+    dcc.Dropdown(id="stats_dd",
+                 options=[
+                     {'label': i, 'value': i} for i in stats
+                 ],
+                 placeholder="wybierz filtr",
+                 style={'width':'40%'}
+
+                 ),
+    dcc.Dropdown(id='stats_value_sign',
+                          options=[
+            {'label': 'more_than', 'value': '>'},
+            {'label': 'equal', 'value': '='},
+            {'label': 'less_than', 'value': '<'}
+        ],),
+    
+    
+    dcc.Input(id='stats_value',
+                          type='text',
+                          value='',
+                          
+                            placeholder='Wpisz wartosć'),
+    html.Button('Wstaw',id='stats_dd_button', n_clicks=0),
+    html.Div(id='score_container'),
+    html.Div(id='score_table'),
+    
+    
+    dt.DataTable(id='filters_table', columns=[{'id':'tabela', 'name':'tabela'},
+                                                {'id':'filtr', 'name':'filtr'},
+                                              {'id':'dane', 'name':'dane'}], data=[],
+                                     row_deletable=True),
+    
+    html.Button('Pokaż dane',id='show_data_button', n_clicks=0),
     html.Div(id='sql_table'),
     
     
    
 
 ])
-
 
 
 
@@ -156,18 +216,17 @@ def update_table(n_clicks, value):
     
     db_execute=[]
 
-    x=value
+    name={'player':value}
 
-   
+    query_result=[]
 
     
 
     if n_clicks>0:
-        query_result=[]
         try:
             conn = psycopg2.connect(DATABASE_URL, sslmode='require')
             cursor = conn.cursor()
-            cursor.execute("SELECT name,age, nations.nationality_name, clubs.club_name, player_score.* FROM players, nations, clubs, player_score WHERE players.name = %s AND players.nationality_id=nations.id AND players.club_id=clubs.id AND player_score.player_id=players.id" ,[x])
+            cursor.execute("SELECT name,age, nations.nationality_name, clubs.club_name, player_score.* FROM players, nations, clubs, player_score WHERE players.name = :player AND players.nationality_id=nations.id AND players.club_id=clubs.id AND player_score.player_id=players.id" ,name)
             db_execute=cursor.fetchall()
             query_result = [dict(line) for line in
                         [zip([column[0] for column in cursor.description], row) for row in db_execute]]
@@ -179,7 +238,7 @@ def update_table(n_clicks, value):
         finally:
             if conn is not None:
                 conn.close()
-        
+       
         db_df = pd.DataFrame(query_result)
    
 
@@ -189,175 +248,138 @@ def update_table(n_clicks, value):
 
 
 
-@app.callback(Output(component_id='nations_dd', component_property='children'),
-              [Input(component_id='nations_dd_button', component_property='n_clicks')],
-              [State(component_id='nations_dd', component_property='value')])
 
 
-def input_nationality(n_clicks, value):
-    if n_clicks>0:
-        
-        sql_dict['nationality_name']=str(value)
-        
+                        
+@app.callback(Output('filters_table', 'data'),
+               
+              [Input('age_button', 'n_clicks'),
+                Input('nations_dd_button', 'n_clicks'),
+               Input('clubs_dd_button', 'n_clicks'),
+               Input('score_dd_button', 'n_clicks'),
+               Input('stats_dd_button', 'n_clicks')],
+              [State('age_value_sign', 'value'),
+               State('age_value', 'value'),
+               State('nations_dd', 'value'),
+               State('clubs_dd', 'value'),
+               State('score_dd', 'value'),
+               State('score_value_sign', 'value'),
+               State('score_value', 'value'),
+               State('stats_dd', 'value'),
+               State('stats_value_sign', 'value'),
+               State('stats_value', 'value'),
+               
+               State('filters_table', 'data')])
+               
+
+def update_filters(age_button, nations_dd_button, clubs_dd_button, score_dd_button, stats_dd_button, age_sign, value_age,  value_nations, value_club, score_name,score_sign, score_value, stats_name,stats_sign, stats_value , existing_data):
+    ctx = dash.callback_context
     
-@app.callback(Output(component_id='clubs_dd', component_property='children'),
-              [Input(component_id='clubs_dd_button', component_property='n_clicks')],
-              [State(component_id='clubs_dd', component_property='value')])
-
-
-def input_clubs(n_clicks, value):
-    if n_clicks>0:
-        
-        sql_dict['club_name']=str(value)
-        
+    button_id=''
     
-    
-
+    if not ctx.triggered:
         
-
-@app.callback(Output(component_id='stats_container', component_property='children'),
-              [Input(component_id='stats_dd_button', component_property='n_clicks')],
-              [State(component_id='stats_dd', component_property='value')])
-
-
-def input_stats(n_clicks, value):
+        button_id=''
     
-    new_stats=html.Div(children=[
-              html.H4(children='Podaj wartosć'),
-              dcc.Dropdown(id='value_sign',
-                          options=[
-            {'label': 'more_than', 'value': '>'},
-            {'label': 'equal', 'value': '='},
-            {'label': 'less_than', 'value': '<'}
-        ],),
-                dcc.Input(id='stats_value',
-                          type='text',
-                          value='',
-                          
-                            placeholder='Wpisz wartosć'),
-                
-
-              html.Button('Dodaj', id='stats_values_add_button', n_clicks=0),
-              dcc.Input(id='control_2', value=value)
-
-                ])
-
-    if n_clicks > 0:
+    else:
+        
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    
+        if button_id=='age_button':
             
-            return new_stats
-
-@app.callback(Output(component_id='stats_table',component_property='children'),
-              [Input(component_id='stats_values_add_button', component_property='n_clicks')],
-               [State('value_sign','value'),
-                State('stats_value','value'),
-                State('control_2','value')])
-
-def define_stats_range(n_clicks, value_sign, stats_value, control_2):
-    if n_clicks>0:
-        stats_value=''.join([value_sign, stats_value])
-        sql_dict[control_2]=str(stats_value)
-
-
-
-
-
-
-@app.callback(Output(component_id='filter_table',component_property='children' ),
-              [Input(component_id='show_filters', component_property='n_clicks')])
+            existing_data.append({'tabela':'players',
+                                    'filtr':'age',
+                                 'dane':str(age_sign)+value_age})
+        
+        elif button_id=='nations_dd_button':
+            
+            existing_data.append({'tabela':'nations',
+                                    'filtr':'nationality_name',
+                                 'dane':value_nations})
+        
+        elif button_id=='clubs_dd_button':
+           
+            existing_data.append({'tabela':'clubs',
+                                    'filtr':'club_name',
+                                 'dane':value_club})
+            
+        elif button_id=='score_dd_button':
+           
+            existing_data.append({'tabela':'player_score',
+                                    'filtr':score_name,
+                                 'dane':str(score_sign)+score_value})
+            
+        elif button_id=='stats_dd_button':
+           
+             existing_data.append({'tabela':'player_stats',
+                                 'filtr':stats_name,
+                                 'dane':str(stats_sign)+stats_value})
              
-def show_filters(n_clicks):
-    
-    
-    
-    sql_temp_dict=copy.deepcopy(sql_dict)
-    filters_df = pd.DataFrame([sql_temp_dict])
-    sql_dict.clear()
-    
-    
-    select='SELECT players.name, '
-    fromT=' FROM players '
-    joinT=' INNER JOIN '
-    whereAsk=' WHERE '
    
-    tables=[]
-    
-    joins=[]
-    where=[]
-    
-    query=''
-    conditionDict={'nations':'nations.id=players.nationality_id',
-               'clubs':'clubs.id=players.club_id',
-               'player_score':'players.id=player_score.player_id',
-               'player_stats':'players.id=player_stats.playerid',
-               'players':'age'}
-    for k in sql_schema_dict:
-        for v in sql_schema_dict[k]:
-            for key in sql_temp_dict.keys():
-                if key==v:
-                    tables.append(k+'.'+v)
-                    joins.append('INNER JOIN '+k+' ON '+ conditionDict[k])
-                    if k in ['player_score', 'player_stats']:
-                        
-                        where.append(' '+v+sql_temp_dict[v])
-                    
-                        
-                    else:
-                        if conditionDict[k]=='age':
-                            where.append(' '+v+sql_temp_dict[v])
-                        else:
-                            condition=sql_temp_dict[v]
-                            conditionQuote=f"'{condition}'"
-                            where.append(' '+v+'='+conditionQuote)
-                    
-                        
-                        
-                    
-
-    joins_unique=np.unique(joins)
-    selectedTables=','.join(tables)
-    selectedJoins=' '.join(joins_unique)
-    selectedJoins=selectedJoins.replace('INNER JOIN players ON age','')
-    selectedWhere=' AND '.join(where)
-    allWhere=whereAsk+selectedWhere
-    query=select+selectedTables+fromT+selectedJoins+allWhere
-    
-    
-    
-    if n_clicks>0:
-        
-        sql_temp_dict.clear()
-        
-
-        data = filters_df.to_dict('records')
-        columns = [{"name": i, "id": i, } for i in (filters_df.columns)]
-        return html.Div([dt.DataTable(data=data, columns=columns),
-                         html.Button('Podkaż dane', id='show_data_button', n_clicks=0),
-                         dcc.Input(id='query', value=query, style={'display':'none'})])
-
-@app.callback(Output(component_id='sql_table',component_property='children' ),
-              [Input(component_id='show_data_button', component_property='n_clicks'),
-               Input('query', 'value')])
+       
+    return  existing_data
               
 
+@app.callback(Output('sql_table','children'),
+              [Input('show_data_button','n_clicks'),
+               Input('filters_table','data')])
 
-def show_data(n_clicks, value):
-    
-    
-    
+def show_data(n_clicks, data):
+    constraintDict={'nations':'nations.id=players.nationality_id',
+               'clubs':'clubs.id=players.club_id',
+               'player_score':'players.id=player_score.player_id',
+               'player_stats':'players.id=player_stats.player_id',
+               'players':'age'}
+    sql_dict={}
+    tables=[]
+    joins=[]
+    whereConditions=[]
     db_search_execute=[]
-
+    query_result_search=[]
+    query=''
+    select="SELECT players.name,"
+    fromQuery=" FROM players "
+    join=" INNER JOIN "
+    on=" ON "
+    where=" WHERE "
+    
+    
     if n_clicks>0:
         
-        sql_dict.clear()
-         
-        query_result_search=[]
         
         
+        for d in data:
+            for k in d.keys():
+                sql_dict[d[list(d)[0]]]=[{d[list(d)[1]]: d[list(d)[2]]}]
+        
+        for k  in sql_dict.keys():
+            for v in sql_dict[k]:
+                for key in v.keys():
+                    if k=='players':
+                        tables.append(str(k)+'.'+str(key))
+                        joins.append(' ')
+                        whereConditions.append(str(key)+v[key])
+                    elif k in ['player_score', 'player_stats']:
+                        tables.append(str(k)+'.'+str(key))
+                        joins.append(join+ str(k) + on + constraintDict[k])
+                        whereConditions.append(str(key)+v[key])
+                    else:
+                        tables.append(str(k)+'.'+str(key))
+                        joins.append(join+ str(k) + on + constraintDict[k])
+                        condition=v[key]
+                        conditionQuote=f"'{condition}'"
+                        whereConditions.append(str(key)+'='+conditionQuote)
+        
+            
+        selectedTables=', '.join(tables)
+        selectedJoins=''.join(joins)
+        selectedWhere= ' AND '.join(whereConditions)
+        query=select+selectedTables+fromQuery+selectedJoins+where+selectedWhere
         
         try:
             conn = psycopg2.connect(DATABASE_URL, sslmode='require')
             cursor = conn.cursor()
-            cursor.execute(value)
+            cursor.execute(query)
             db_search_execute=cursor.fetchall()
             query_result_search= [dict(line) for line in
                         [zip([column[0] for column in cursor.description], row) for row in db_search_execute]]
@@ -369,9 +391,6 @@ def show_data(n_clicks, value):
         finally:
             if conn is not None:
                     conn.close()
-    
-    
-        
         db_df_search = pd.DataFrame(query_result_search)
 
 
@@ -380,12 +399,11 @@ def show_data(n_clicks, value):
         
         if len(data)==0:
             return html.Div(children=[html.A(html.Button('Nowe wyszukiwanie'),href='/'),
-                                          html.H6(children="Twoje zapytanie: "+value + ": Brak danych")])
+                                          html.H6(children="Twoje zapytanie: "+query + ": Brak danych")])
         else:
             return html.Div(children=[html.A(html.Button('Nowe wyszukiwanie'),href='/'),
-                                      html.H6(children="Twoje zapytanie: "+value),
+                                      html.H6(children="Twoje zapytanie: "+query),
                                       dt.DataTable(data=data, columns=columns)])
-     
-    
+        return query
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
